c9f2a32c995fb40725988fa33e0f70c179d35d4d
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-08 17:47:52 +0900

    install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
+import Home from "./components/Home";
+import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/createpost" element={<CreatePost />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/logout" element={<Logout />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
