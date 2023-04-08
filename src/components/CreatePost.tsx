6b39ff5b85b5bb834f9c965413e28c17a63506da
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-09 01:06:54 +0900

    post

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,20 +1,73 @@
 import React from "react";
 import "./CreatePost.css";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+  const [title, setTitle] = React.useState<string>("");
+  const [postText, setPostText] = React.useState<string>("");
+
+  const navigate = useNavigate();
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handlePostTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPostText(e.target.value);
+  };
+
+  const createPost = async () => {
+    await addDoc(collection(db, "posts"), {
+      title: title,
+      postText: postText,
+      author: {
+        username: auth.currentUser?.displayName,
+        uid: auth.currentUser?.uid,
+      },
+      createdAt: new Date(),
+    })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      })
+      .finally(() => {
+        console.log("finally");
+      });
+
+    setTitle("");
+    setPostText("");
+
+    alert("投稿しました");
+
+    navigate("/");
+  };
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div className="inputPost">
           <div>タイトル</div>
-          <input type="text" placeholder="タイトルを記入" />
+          <input
+            type="text"
+            placeholder="タイトルを記入"
+            onChange={handleTitleChange}
+          />
         </div>
         <div className="inputPost">
           <div>投稿</div>
-          <textarea placeholder="投稿内容を記入"></textarea>
+          <textarea
+            placeholder="投稿内容を記入"
+            onChange={handlePostTextChange}
+          ></textarea>
         </div>
-        <button className="postButton">投稿する</button>
+        <button className="postButton" onClick={createPost}>
+          投稿する
+        </button>
       </div>
     </div>
   );
