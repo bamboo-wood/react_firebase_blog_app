a2f3442007c923ba9cf4bcf6295dd3894a1a049f
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-08 18:19:30 +0900

    set up icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,30 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faFilePen,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
   console.log("Navbar");
 
   return (
     <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/createpost"}>Post</Link>
-      <Link to={"/login"}>Login</Link>
+      <Link to={"/"}>
+        <FontAwesomeIcon icon={faHome} />
+        Home
+      </Link>
+      <Link to={"/createpost"}>
+        <FontAwesomeIcon icon={faFilePen} />
+        Post
+      </Link>
+      <Link to={"/login"}>
+        <FontAwesomeIcon icon={faArrowRightToBracket} />
+        Login
+      </Link>
     </nav>
   );
 };
