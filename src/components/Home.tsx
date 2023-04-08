bdb43424bc22faa7a1a14ba8b4addd2c68e134cd
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-09 01:10:19 +0900

    html

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>Title</h1>
+        </div>
+      </div>
+      <div className="postTextContainer">
+        今はReactの学習中です。これから頑張ります。
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@ShinCode</h3>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
